914e4e0a0ae668619702be30953e0b5d2346785d
unknown <ecorrea424@gmail.com>
2024-12-09 19:40:06 -0800

    Final Project: Routing Lab

--- app.js
+++ app.js
@@ -13,8 +13,37 @@ app.listen(PORT, () => {
     debug(`listening on port ${chalk.blue(PORT)}`);
   });
   
-
-
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+// ------------ ROUTES ------------ //
+
+//Main Page
+app.get('/', (req, res) => {
+  res.send('<h1>Houses currently on the market</h1>');
+});
+
+//Add House
+app.get('/house/add', (req, res) => {
+  res.redirect('/');
+});
+
+//Delete House 
+app.get('/house/:houseId/delete', (req, res) => {
+  res.redirect('/');
+});
+
+// House Bids
+app.get('/house/:houseId/bids', (req, res) => {
+  res.send('<h1>Bids of house displayed here</h1>');
+});
+
+//Add a Bid for a specified House
+app.get('/house/:houseId/bid/add', (req,res) => {
+  res.redirect('/house/:houseId/bids')
+});
+
+//Remove a Bid on a specified house
+app.get('/house/:houseId/bid/:bidId/delete', (req,res) => {
+  res.redirect('/house/:houseId/bids')
+});
