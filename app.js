d5f7ea3198136b7abf83f14897303ace114e2d92
unknown <ecorrea424@gmail.com>
2024-12-10 21:41:55 -0800

    Final Project: Templating Engines Lab

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const express = require("express");
 const path = require("path");
-
-
+const app = express();
 
 
 const PORT = process.env.PORT || 4000;
-const app = express();
+
 app.listen(PORT, () => {
     debug(`listening on port ${chalk.blue(PORT)}`);
   });
@@ -16,11 +15,22 @@ app.listen(PORT, () => {
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "/public/")));
 
+app.use(
+  "/css",
+  express.static(
+    path.join(__dirname, "node_modules", "bootstrap", "dist", "css")
+  )
+);
+
+
+app.set("views", path.join(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
 // ------------ ROUTES ------------ //
 
 //Main Page
 app.get('/', (req, res) => {
-  res.send('<h1>Houses currently on the market</h1>');
+  res.render('index');
 });
 
 //Add House
@@ -35,15 +45,16 @@ app.get('/house/:houseId/delete', (req, res) => {
 
 // House Bids
 app.get('/house/:houseId/bids', (req, res) => {
-  res.send('<h1>Bids of house displayed here</h1>');
+  res.render("houseBids");
 });
 
 //Add a Bid for a specified House
 app.get('/house/:houseId/bid/add', (req,res) => {
-  res.redirect('/house/:houseId/bids')
+  res.render("houseBids")
 });
 
 //Remove a Bid on a specified house
 app.get('/house/:houseId/bid/:bidId/delete', (req,res) => {
-  res.redirect('/house/:houseId/bids')
+  res.render("houseBids")
 });
+
